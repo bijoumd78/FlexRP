dd9240ea87dc88eb3cd21a43c153aede432938df
Mahamadou <bijoumd@gmail.com>
2019-08-20 19:21:15 -0400

    Fixed minor formatting issues

--- test/test_FlexRPSharedMemory.cpp
+++ test/test_FlexRPSharedMemory.cpp
@@ -10,15 +10,12 @@ TEST_CASE( "Get recon module property values", "Properties" )
     static std::vector<std::string> properties{"1", "3.14", "false", "Hello unit test"};
     FlEXRP::FlexRPSharedMemory::createSharedMemory(properties);
 
-
     REQUIRE( properties.size() == 4);
 
     static std::vector<std::string> values;
     FlEXRP::FlexRPSharedMemory::getReconmoduleProperty(values, 4);
 
     SECTION( "Get property values" ) {
-
-
         REQUIRE(  std::stoi(values[0] ) == 1 );
         REQUIRE(  std::stof(values[1] ) == Approx( 3.14f ) );
         REQUIRE(  FlEXRP::FlexRPSharedMemory::to_bool(values[2]) == false );
